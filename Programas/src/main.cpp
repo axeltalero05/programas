24c9bf62d32022fb35743ae0fefb37370b9cfe08
axeltalero5 <axel.talero@cun.edu.co>
2019-08-27 16:36:52 -0500

    HOLA MUNDO
    Programa Prueba

--- Programas/src/main.cpp
+++ Programas/src/main.cpp
@@ -1,15 +1,10 @@
 #include <Arduino.h>
 
-#define LED 2
-
 void setup() {
-  pinMode(LED,OUTPUT);
-  digitalWrite(LED,LOW);
+ Serial.begin(9600);
+ Serial.println("Hla Mundo");
 }
 
 void loop() {
-  digitalWrite(LED,HIGH);
-  delay(500);
-  digitalWrite(LED,LOW);
-  delay(500);
+  
 }
